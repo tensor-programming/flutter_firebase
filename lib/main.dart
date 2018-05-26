52ac441b8bf3ca6ffe72551fb43fdc25f3e2179d
Tensor-Programming <abeltensor@tensor-programming.com>
2018-05-26 14:21:59 -0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-mport 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_database/ui/firebase_animated_list.dart';
